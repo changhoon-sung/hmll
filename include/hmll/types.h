8a95f2228180c7e8bdf7a62c71c105e3444c3e18
Funtowicz Morgan <mfuntowicz@users.noreply.github.com>
2026-01-02 14:00:44 +0100

    Enable reading chunked safetensors files (#8)
    * feat(safetensors): support chunked safetensors
    
    * feat(python): update to match git uniformization
    
    * feat(iouring): fix memory leak
    
    * feat(safetensors): expose all the dtype
    
    * feat(safetensors): define hmll_safetensors_read_index in safetensors header
    
    * misc: scope fds registration in iouring
    
    * misc(actions): cleanup a bit the workflow
    
    * misc(actions): add setup-python step in python workflow
    
    * misc: remove unwanted file
    
    * test(dtype): cover all the dtypes
    
    * feat(safetensors): handle single file unallocated structures

--- include/hmll/types.h
+++ include/hmll/types.h
@@ -31,6 +31,7 @@ enum hmll_error_code
 
     HMLL_ERR_SAFETENSORS_JSON_INVALID_HEADER = -30,
     HMLL_ERR_SAFETENSORS_JSON_MALFORMED_HEADER = -31,
+    HMLL_ERR_SAFETENSORS_JSON_MALFORMED_INDEX = -32,
 
     HMLL_ERR_CUDA_NOT_ENABLED = -40,
     HMLL_ERR_CUDA_NO_DEVICE = -41,
@@ -68,15 +69,34 @@ typedef enum hmll_flags hmll_flags_t;
 enum hmll_file_kind
 {
     HMLL_SAFETENSORS,
+    HMLL_SAFETENSORS_CHUNKED,
     HMLL_GGUF
 };
 typedef enum hmll_file_kind hmll_file_kind_t;
 
 enum hmll_tensor_data_type
 {
+    HMLL_DTYPE_BOOL,
     HMLL_DTYPE_BFLOAT16,
+    HMLL_DTYPE_COMPLEX,
+    HMLL_DTYPE_FLOAT4,
+    HMLL_DTYPE_FLOAT6_E2M3,
+    HMLL_DTYPE_FLOAT6_E3M2,
+    HMLL_DTYPE_FLOAT8_E5M2,
+    HMLL_DTYPE_FLOAT8_E4M3,
+    HMLL_DTYPE_FLOAT8_E8M0,
     HMLL_DTYPE_FLOAT16,
     HMLL_DTYPE_FLOAT32,
+    HMLL_DTYPE_SIGNED_INT4,
+    HMLL_DTYPE_SIGNED_INT8,
+    HMLL_DTYPE_SIGNED_INT16,
+    HMLL_DTYPE_SIGNED_INT32,
+    HMLL_DTYPE_SIGNED_INT64,
+    HMLL_DTYPE_UNSIGNED_INT4,
+    HMLL_DTYPE_UNSIGNED_INT8,
+    HMLL_DTYPE_UNSIGNED_INT16,
+    HMLL_DTYPE_UNSIGNED_INT32,
+    HMLL_DTYPE_UNSIGNED_INT64,
     HMLL_DTYPE_UNKNOWN
 };
 typedef enum hmll_tensor_data_type hmll_tensor_data_type_t;
@@ -95,13 +115,15 @@ struct hmll_tensor_lookup_result
 {
     struct hmll_tensor_specs specs;
     size_t index;
-    int found;
+    unsigned short fidx;
+    unsigned char found;
 };
 typedef struct hmll_tensor_lookup_result hmll_tensor_lookup_result_t;
 
 struct hmll_table
 {
     struct hmll_tensor_specs *tensors;
+    unsigned short *indexes;
     char **names;
 };
 typedef struct hmll_table hmll_table_t;
@@ -123,8 +145,9 @@ struct hmll_device_buffer
 typedef struct hmll_device_buffer hmll_device_buffer_t;
 
 struct hmll_context {
-    struct hmll_source source;
     struct hmll_table table;
+    struct hmll_source *sources;
+    size_t num_sources;
     size_t num_tensors;
     enum hmll_file_kind kind;
     enum hmll_error_code error;
